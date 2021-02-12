3fdecac982bc0010235646c647d0a36beead890b
Milo123459 <milo5huntington@gmail.com>
2021-02-12 14:04:28 +0000

    start of cli

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <h2 align="center">
 Next generation <b>Git</b> tooling.
 <h2>
-<h3 align="center">
+<p align="center">
 A simple command that allows you to <b>fetch</b>, <b>add</b>, <b>commit</b> and <b>push</b>.
-</h3>
+</p>
 <h1></h1>
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import chalk from 'chalk';
+import meow from 'meow';
+import terminalLink from 'terminal-link';
+import { execSync } from 'child_process';
+import fs from 'fs/promises';
+
+const cli = meow(`
+See the ${terminalLink('readme', 'https://github.com/Milo123459/glitter')} for information.
+`, {
+    flags: {
+        branch: {
+            type: 'string',
+            isRequired: false,
+            default: execSync('git rev-parse --abbrev-ref HEAD').toString().split(/\n/gi)[0],
+            alias: 'b'
+        },
+        commit: {
+            type: 'string',
+            isRequired: true,
+            alias: 'c'
+        },
+        args: {
+            type: 'string',
+            isRequired: false,
+            alias: 'a'
+        }
+    }
+})
+
+console.log(cli.flags);
