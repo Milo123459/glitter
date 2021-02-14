59755923ca8473d848a726060dbad0db93da78dc
Suyashtnt <suyashtnt@gmail.com>
2021-02-14 14:53:12 +0200

    fix: fix when an argument is used multiple times feat: add tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,10 @@
 use std::io::Error;
 
+use onig::Regex;
+
 use crate::config::{Arguments, GlitterRc};
 
-fn push(config: GlitterRc, args: Arguments) -> anyhow::Result<()> {
+fn push(config: GlitterRc, args: Arguments) -> anyhow::Result<String> {
     if config.commit_message == "$RAW_COMMIT_MSG" {
         return Err(anyhow::Error::new(Error::new(
             std::io::ErrorKind::InvalidInput,
@@ -18,6 +20,14 @@ fn push(config: GlitterRc, args: Arguments) -> anyhow::Result<()> {
         if val.len() >= 2 && String::from(val.chars().nth(1).unwrap()) == String::from("+") {
             let idx = val.chars().nth(0).unwrap().to_digit(10).unwrap() - 1;
             let rest = &args.arguments[idx as usize..];
+
+            if rest.len() == 0 {
+                return Err(anyhow::Error::new(Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    format!("Argument ${} was not provided", val),
+                )));
+            }
+
             result = result.replace(
                 &format!("${}+", String::from(val).split("").collect::<Vec<_>>()[1]),
                 &rest.join(" "),
@@ -32,19 +42,20 @@ fn push(config: GlitterRc, args: Arguments) -> anyhow::Result<()> {
                 )));
             }
 
-            let val_ = &args.arguments[idx];
-            result = result.replace(
-                &format!("${}", String::from(val).split("").collect::<Vec<_>>()[1]),
-                &val_,
-            );
+            let val_ = &*args.arguments[idx];
+            result = Regex::new(&format!(
+                "\\${}(?!@)",
+                String::from(val).split("").collect::<Vec<_>>()[1]
+            ))?
+            .replace(&result, val_)
         }
     }
 
     println!("{}", result);
-    Ok(())
+    Ok(result)
 }
 
-pub fn match_cmds(args: Arguments, config: GlitterRc) -> anyhow::Result<()> {
+pub fn match_cmds(args: Arguments, config: GlitterRc) -> anyhow::Result<String> {
     let cmd = &args.action;
     match &*cmd.to_lowercase() {
         "push" => push(config, args),
@@ -52,5 +63,72 @@ pub fn match_cmds(args: Arguments, config: GlitterRc) -> anyhow::Result<()> {
             std::io::ErrorKind::InvalidInput,
             "Invalid action. Can only be `push`",
         ))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::config::{Arguments, GlitterRc};
+
+    use super::push;
+
+    #[test]
+    fn basic() {
+        let args = Arguments {
+            action: "push".to_string(),
+            arguments: vec![
+                "test".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+            ],
+            rc_path: PathBuf::new(),
+        };
+
+        let config = GlitterRc {
+            commit_message: "$1($2): $3+".to_string(),
+            commit_message_arguments: None,
+        };
+
+        assert_eq!(push(config, args).unwrap(), "test(a): b c")
+    }
+
+    #[test]
+    fn reuse_arguments() {
+        let args = Arguments {
+            action: "push".to_string(),
+            arguments: vec![
+                "test".to_string(),
+                "a".to_string(),
+                "b".to_string(),
+                "c".to_string(),
+            ],
+            rc_path: PathBuf::new(),
+        };
+
+        let config = GlitterRc {
+            commit_message: "$1($2): $3+ : $2 | $1+".to_string(),
+            commit_message_arguments: None,
+        };
+
+        assert_eq!(push(config, args).unwrap(), "test(a): b c : a | test a b c")
+    }
+
+    #[test]
+    fn less_than_needed_args() {
+        let args = Arguments {
+            action: "push".to_string(),
+            arguments: vec!["test".to_string(), "a".to_string()],
+            rc_path: PathBuf::new(),
+        };
+
+        let config = GlitterRc {
+            commit_message: "$1($2): $3+".to_string(),
+            commit_message_arguments: None,
+        };
+
+        assert_eq!(push(config, args).is_err(), true)
     }
 }
