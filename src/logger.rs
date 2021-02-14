64ef5fc938226faa42629b7309aad3611bb27f1d
Suyashtnt <suyashtnt@gmail.com>
2021-02-14 13:13:01 +0200

    refactor: general refactior and use Structopt
    Structopt is used as it is way easier to work with

--- src/logger.rs
+++ src/logger.rs
@@ -1,12 +1,20 @@
-pub fn error(input: &str) {
-    println!("FATAL {}", input);
+pub struct Logger;
+
+impl Logger {
+    pub fn error(input: &str) {
+        println!("FATAL {}", input);
+    }
+    
+    pub fn info(input: &str) {
+        println!("INFO {}", input);
+    }
+
+    pub fn warn(input: &str) {
+        println!("WARN {}", input);
+    }
+
+    pub fn success(input: &str) {
+        println!("SUCCESS {}", input);
+    }
 }
-pub fn info(input: &str) {
-    println!("INFO {}", input);
-}
-pub fn warn(input: &str) {
-    println!("WARN {}", input);
-}
-pub fn success(input: &str) {
-    println!("SUCCESS {}", input);
-}
+
