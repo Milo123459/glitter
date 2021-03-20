560a54999840a576a623282ceaae224c5c102bfa
Milo <50248166+Milo123459@users.noreply.github.com>
2021-03-20 23:37:14 +0000

    feat: cli: it now works without needing a config file

--- src/get_and_parse.rs
+++ src/get_and_parse.rs
@@ -9,7 +9,17 @@ pub fn parse(path: &PathBuf) -> anyhow::Result<GlitterRc> {
 
     match serde_json::from_reader(file) {
         Ok(json) => Ok(json),
-        Err(err) => Err(anyhow::Error::new(err)).with_context(|| "error parsing glitterrc"),
+        Err(err) => {
+            println!("GlitterRC doesn't exist, {:?}", err);
+            Ok(GlitterRc {
+                fetch: None,
+                commit_message: "$1+".to_owned(),
+                arguments: None,
+                custom_tasks: None,
+                commit_message_arguments: None
+            })
+
+        }
     }
 }
 // tests
