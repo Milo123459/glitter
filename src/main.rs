22f01de8520ef183e68e529b6a149b6f128ff85d
Just a Badly Drawn TABS Dude <45307955+Suyashtnt@users.noreply.github.com>
2021-08-28 15:27:42 +0200

    tests: all: remove all tests

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,3 @@ fn main() -> anyhow::Result<()> {
 
 	Ok(())
 }
-// tests
-#[cfg(test)]
-mod tests {
-	use super::main;
-
-	#[test]
-	fn runs_correctly() {
-		// main will always error as it doesnt get any args
-		assert!(main().is_err());
-	}
-}
