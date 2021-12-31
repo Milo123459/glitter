575055596f8b2f1eb930ae4edaf9905ce5ef162c
Milo <50248166+Milo123459@users.noreply.github.com>
2021-12-31 05:58:33 +0000

    fix: cli: remove dumb clone

--- src/cli.rs
+++ src/cli.rs
@@ -343,7 +343,7 @@ pub fn push(
 				format!("{}{}{0}", "`".green(), sha.green())
 			)),
 		);
-		println!("{:?}", diff.clone());
+		println!("{:?}", diff);
 
 		if let Some(diff) = diff {
 			let split = diff.split('\n');
