c7145a951a182d6512c0dc4c553bb584def041ab
figsoda <figsoda@pm.me>
2022-08-05 10:33:46 +0800

    fix: cli: exit peacefully on --help and --version

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ use glitter::config::Arguments;
 use structopt::StructOpt;
 
 fn main() -> anyhow::Result<()> {
-	glitter::run(Arguments::from_args_safe()?)?;
-
-	Ok(())
+	match Arguments::from_args_safe() {
+		Ok(args) => glitter::run(args),
+		Err(err) => {
+			if err.use_stderr() {
+				Err(err.into())
+			} else {
+				println!("{}", err.message);
+				Ok(())
+			}
+		}
+	}
 }
 // tests
 #[cfg(test)]
